ae8ab8fb804bb4f087453474f8d49d2587de0b4d
JinfuC <43752841+JinfuC@users.noreply.github.com>
2024-10-23 16:19:17 +0200

    def datatypes + hpp of StockManager

---
+++ src/backend/StockManager.hpp
@@ -0,0 +1,8 @@
+#include <string>
+class StockManager {
+    public:
+        bool palletFitsInStock();
+        bool getFreeStock();
+        string placeInStock();
+        string removeFromStock();
+};
